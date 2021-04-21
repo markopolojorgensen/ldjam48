07847a78003137f61b29aaac5e2827d82c5f5b96
markopolodev <marko.polo.jorgensen@gmail.com>
2021-04-21 00:10:34 -0500

    palette & logo intro

---
+++ logo_intro.gd
@@ -0,0 +1,12 @@
+extends PanelContainer
+
+export(bool) var skip = false
+
+func _ready():
+	if skip:
+		hide()
+	else:
+		$center_container/control/animated_sprite.frame = 0
+		yield(get_tree().create_timer(1), "timeout")
+		$animation_player.play("show_logo")
+
