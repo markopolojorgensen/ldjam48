9f5fed9f5dd761339da9b01615bde6f5d660edff
markopolodev <marko.polo.jorgensen@gmail.com>
2021-04-21 01:34:32 -0500

    more boilerplate: logo sfx & fullscreen toggle

--- global.gd
+++ global.gd
@@ -3,4 +3,8 @@ extends Node
 func _unhandled_input(event):
 	if event.is_action_pressed("instaquit"):
 		get_tree().quit()
+	
+	if event.is_action_pressed("toggle_fullscreen"):
+		OS.window_fullscreen = not OS.window_fullscreen
+
 
