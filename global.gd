3c41f7a7ac436ae91bf61f55e2d1dd591ac525b7
markopolodev <marko.polo.jorgensen@gmail.com>
2021-04-23 11:20:05 -0500

    F1 to screenshot

--- global.gd
+++ global.gd
@@ -12,7 +12,29 @@ func _unhandled_input(event):
 	if event.is_action_pressed("toggle_mute"):
 		muted = not muted
 		update_audio()
+	
+	if event.is_action_pressed("screenshot"):
+		screenshot()
 
 func update_audio():
 	AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), muted)
+
+func screenshot():
+	# get screen capture
+	var capture = get_viewport().get_texture().get_data()
+	capture.flip_y()
+	
+	# get unused screenshot number
+	var file_checker = File.new()
+	var i = 0
+	var file_name = "user://screenshot_%03d.png" % i
+	# only supports 1000 screenshots?
+	while file_checker.file_exists(file_name):
+		i += 1
+		file_name = "user://screenshot_%03d.png" % i
+	
+	# save to a file
+	capture.save_png(file_name)
+	
+	print("screenshot saved in %s  (%s)" % [str(OS.get_user_data_dir()), file_name])
 
--- logo_intro.gd
+++ logo_intro.gd
@@ -5,11 +5,13 @@ export(bool) var skip = false
 func _ready():
 	if skip:
 		hide()
+		queue_free()
 	else:
 		show()
 		$center_container/control/animated_sprite.frame = 0
 		yield(get_tree().create_timer(1), "timeout")
 		$animation_player.play("show_logo")
+		$lifetime.start()
 
 func _on_lifetime_timeout():
 	queue_free()
